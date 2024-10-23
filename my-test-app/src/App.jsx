d1ce66075b75a0ae8e4c1fe65fc7172ee5c69b7b
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2024-10-23 14:36:37 +0545

    vite for testing and some patch updates

---
+++ my-test-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Chimebox  from 'react-chimebox'
+
+function App() {
+  return (
+      <div className="App">
+          <h1>My Test App</h1>
+          <Chimebox siteId="1e13e3d5-abbe-4d69-8442-6b2889d39911" />
+      </div>
+  );
+}
+
+export default App;
