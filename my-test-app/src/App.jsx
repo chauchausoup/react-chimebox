2d89adcb8f09b53371bbf805d5501e8794ac7436
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2024-10-30 17:20:05 +0545

    new patch for proper setting type

--- my-test-app/src/App.jsx
+++ my-test-app/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import  Chimebox  from 'react-chimebox'
+import CommentWidget from 'react-chimebox'
 
 function App() {
   return (
       <div className="App">
           <h1>My Test App</h1>
 					{/*org45.netlify.app keys*/}
-          <Chimebox siteId="e641328e-f5fd-4ee4-ad2a-eea64a58406f" turnstileDataSiteKey="0x4AAAAAAAyyLs9gui2lJm5y" />
+          <CommentWidget siteId="e641328e-f5fd-4ee4-ad2a-eea64a58406f" turnstileDataSiteKey="0x4AAAAAAAyyLs9gui2lJm5y" />
       </div>
   );
 }
